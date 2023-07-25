30ab5fffd1bd5433ad2041fd261b0c624ad1e060
Vaibhavpatil99 <patilvb1999@gmail.com>
2023-07-25 17:14:52 +0530

    changes done

--- furniture/settings.py
+++ furniture/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u!=gmqkfw-$gw0aqw4qpr+=7k0)qe11z0syf18kwbq6%ub*$r3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
