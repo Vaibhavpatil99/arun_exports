06c3b23df16185ac33f9384dd2e16b1549db89e1
Vaibhavpatil99 <patilvb1999@gmail.com>
2023-08-05 15:37:25 +0530

    rr

--- furniture/settings.py
+++ furniture/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'furniture.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'TEMPLATES')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
